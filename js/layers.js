24a8bac2e9a65293907ccea59f75e8184ddc3a66
ydnaback1 <70602147+ydnaback1@users.noreply.github.com>
2025-05-16 12:55:53 +0100

    Update offline fix layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,14 @@
 function getBaseLayers(serviceUrl, apiKey) {
     return {
-        'OS Leisure': L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 9, attribution: '&copy; Ordnance Survey' }),
+        'OS Outdoor': L.tileLayer(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 22, attribution: '&copy; Ordnance Survey' }),
         'OS Road': L.tileLayer(`${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 22, attribution: '&copy; Ordnance Survey' }),
-        'OS Outdoor (Offline)': L.tileLayer.offline(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 22, attribution: '&copy; Ordnance Survey' })
+        'OS Leisure': L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 9, attribution: '&copy; Ordnance Survey' })
     };
+}
+
+function getOfflineLayer(serviceUrl, apiKey) {
+    return L.tileLayer.offline(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
+        maxZoom: 22,
+        tileSize: 256
+    });
 }
