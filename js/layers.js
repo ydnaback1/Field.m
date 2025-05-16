ad86aecca181aa05013fe866aa2823a900ef17be
ydnaback1 <70602147+ydnaback1@users.noreply.github.com>
2025-05-16 13:03:32 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,21 @@
 function getBaseLayers(serviceUrl, apiKey) {
     return {
-        'OS Outdoor': L.tileLayer(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 22, attribution: '&copy; Ordnance Survey' }),
-        'OS Road': L.tileLayer(`${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 22, attribution: '&copy; Ordnance Survey' }),
-        'OS Leisure': L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`, { maxZoom: 9, attribution: '&copy; Ordnance Survey' })
+        'OS Road WMS': L.tileLayer.wms('https://your-wms-server-url', {
+            layers: 'your-layer-name',
+            format: 'image/png',
+            transparent: true,
+        }),
+        'OS Outdoor': L.tileLayer(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
+            maxZoom: 22,
+        }),
+        'OS Leisure': L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
+            maxZoom: 9,
+        })
     };
 }
 
 function getOfflineLayer(serviceUrl, apiKey) {
     return L.tileLayer.offline(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
         maxZoom: 22,
-        tileSize: 256
     });
 }
