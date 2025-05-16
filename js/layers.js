36fb2f14f067bf3f38114df8a550e83767377dbe
ydnaback1 <70602147+ydnaback1@users.noreply.github.com>
2025-05-16 13:28:25 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,11 +1,9 @@
 function getBaseLayers(serviceUrl, apiKey) {
     return {
-        'OS Road WMS': L.tileLayer.wms('https://your-wms-server-url', {
-            layers: 'your-layer-name',
-            format: 'image/png',
-            transparent: true,
-        }),
         'OS Outdoor': L.tileLayer(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
+            maxZoom: 22,
+        }),
+        'OS Road': L.tileLayer(`${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
             maxZoom: 22,
         }),
         'OS Leisure': L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
