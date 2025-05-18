ebdcd65afc65cf3fe0ccd28024630d9cfbfa3ac4
ydnaback1 <70602147+ydnaback1@users.noreply.github.com>
2025-05-18 16:20:18 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,13 +1,25 @@
-function getBaseLayers(serviceUrl, apiKey) {
+// js/layers.js
+
+function getUKBaseLayers(serviceUrl, apiKey) {
     return {
-        'OS Outdoor': L.tileLayer(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
-            maxZoom: 22,
-        }),
-        'OS Road': L.tileLayer(`${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
-            maxZoom: 22,
-        }),
-        'OS Leisure': L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`, {
-            maxZoom: 9,
-        })
+        'OS Road': L.tileLayer(
+            `${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`,
+            { maxZoom: 22, attribution: '&copy; Ordnance Survey' }
+        ),
+        'OS Outdoor': L.tileLayer(
+            `${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`,
+            { maxZoom: 22, attribution: '&copy; Ordnance Survey' }
+        ),
+        'OS Leisure': L.tileLayer(
+            `${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`,
+            { maxZoom: 9, attribution: '&copy; Ordnance Survey' }
+        )
     };
+}
+
+function getWorldBaseLayer() {
+    return L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { maxZoom: 19, attribution: '&copy; OpenStreetMap contributors' }
+    );
 }
