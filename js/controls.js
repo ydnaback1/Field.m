0d3a93d40935315d01f7e9251c6a426b60d3bfa3
ydnaback1 <70602147+ydnaback1@users.noreply.github.com>
2025-05-19 08:52:20 +0100

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -8,12 +8,25 @@ function addUKControls(map, baseLayers) {
         color: '#FF0080'
     }).addTo(map);
     L.control.layers(baseLayers).addTo(map);
+    L.control.scale({
+        position: 'bottomleft',
+        imperial: false, 
+        metric: true,
+        maxWidth: 200
+    }).addTo(map);
 }
+
 function addWorldControls(map) {
     L.control.locate().addTo(map);
     L.control.measure({
         position: 'topleft',
         collapsed: true,
         color: '#3388ff'
+    }).addTo(map);
+    L.control.scale({
+        position: 'bottomleft',
+        imperial: true,   
+        metric: true,
+        maxWidth: 200
     }).addTo(map);
 }
