c06a05e5221e272f11553b3ad53b2f5055fb6276
ydnaback1 <andy1gilligan@gmail.com>
2026-09-17 16:22:51 +0100

    Enable Overzoom on map layer

--- js/layers.js
+++ js/layers.js
@@ -1,18 +1,24 @@
 // js/layers.js
 
+const UK_BASE_MAX_ZOOM = 22;
+
 function getUKBaseLayers(serviceUrl, apiKey) {
     return {
         'OS Road': L.tileLayer(
             `${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${apiKey}`,
-            { maxZoom: 22, attribution: '&copy; Ordnance Survey' }
+            { maxZoom: UK_BASE_MAX_ZOOM, attribution: '&copy; Ordnance Survey' }
         ),
         'OS Outdoor': L.tileLayer(
             `${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${apiKey}`,
-            { maxZoom: 22, attribution: '&copy; Ordnance Survey' }
+            { maxZoom: UK_BASE_MAX_ZOOM, attribution: '&copy; Ordnance Survey' }
         ),
         'OS Leisure': L.tileLayer(
             `${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${apiKey}`,
-            { maxZoom: 9, attribution: '&copy; Ordnance Survey' }
+            {
+                maxNativeZoom: 9,
+                maxZoom: UK_BASE_MAX_ZOOM,
+                attribution: '&copy; Ordnance Survey'
+            }
         )
     };
 }
